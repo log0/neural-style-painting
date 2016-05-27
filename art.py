b8fcabe6b1c2ebdca71c9ecd49e1de093b0a95dc
echio <Eric Chio "Log0>
2016-05-26 23:31:31 -0700

    Implemented a working version of painter algorithm.

--- art.py
+++ art.py
@@ -1,76 +1,146 @@
+"""
+Implementation of the "A Neural Algorithm of Artistic Style" in Python using
+TensorFlow.
+"""
 import os
 import sys
 import numpy as np
 import scipy.io
 import scipy.misc
-from scipy import ndimage
 import tensorflow as tf
 
+###############################################################################
+# Constants for the image input and output.
+###############################################################################
+
+# Output folder for the images.
+OUTPUT_DIR = 'output/'
+# Style image to use.
+# STYLE_IMAGE = 'images/starry_night.jpg'
 STYLE_IMAGE = 'images/StarryNight.jpg'
+# Content image to use.
+# CONTENT_IMAGE = 'images/hong_kong_2.jpg'
 CONTENT_IMAGE = 'images/Macau.jpg'
+# Image dimensions constants. 
 IMAGE_WIDTH = 800
 IMAGE_HEIGHT = 600
 COLOR_CHANNELS = 3
 
-BETA = 1
-ALPHA = 500
-
+###############################################################################
+# Algorithm constants
+###############################################################################
+# Noise ratio. Percentage of weight of the noise for intermixing with the
+# content image.
+NOISE_RATIO = 0.6
+# Number of iterations to run.
+ITERATIONS = 5000
+# Constant to put more emphasis on content loss.
+BETA = 5
+# Constant to put more emphasis on style loss.
+ALPHA = 100
+# Path to the deep learning model. This is more than 500MB so will not be
+# included in the repository, but available to download at the model Zoo:
+# Link: https://github.com/BVLC/caffe/wiki/Model-Zoo
+#
+# Pick the VGG 19-layer model by from the paper "Very Deep Convolutional 
+# Networks for Large-Scale Image Recognition".
 VGG_MODEL = 'imagenet-vgg-verydeep-19.mat'
+# The mean to subtract from the input to the VGG model. This is the mean that
+# when the VGG was used to train. Minor changes to this will make a lot of
+# difference to the performance of model.
+MEAN_VALUES = np.array([123.68, 116.779, 103.939]).reshape((1,1,1,3))
+
+def generate_noise_image(content_image, noise_ratio = NOISE_RATIO):
+    """
+    Returns a noise image intermixed with the content image at a certain ratio.
+    """
+    noise_image = np.random.uniform(
+            -20, 20,
+            (1, IMAGE_HEIGHT, IMAGE_WIDTH, COLOR_CHANNELS)).astype('float32')
+    # White noise image from the content representation. Take a weighted average
+    # of the values
+    input_image = noise_image * noise_ratio + content_image * (1 - noise_ratio)
+    return input_image
 
 def load_image(path):
     image = scipy.misc.imread(path)
+    # Resize the image for convnet input, there is no change but just
+    # add an extra dimension.
+    image = np.reshape(image, ((1,) + image.shape))
+    # Input to the VGG model expects the mean to be subtracted.
+    image = image - MEAN_VALUES
     return image
+
+def save_image(path, image):
+    # Output should add back the mean.
+    image = image + MEAN_VALUES
+    # Get rid of the first useless dimension, what remains is the image.
+    image = image[0]
+    image = np.clip(image, 0, 255).astype('uint8')
+    scipy.misc.imsave(path, image)
 
 def load_vgg_model(path):
     """
-    0 is conv1_1 (3, 3, 3, 64)
-    1 is relu
-    2 is conv1_2 (3, 3, 64, 64)
-    3 is relu    
-    4 is maxpool
-    5 is conv2_1 (3, 3, 64, 128)
-    6 is relu
-    7 is conv2_2 (3, 3, 128, 128)
-    8 is relu
-    9 is maxpool
-    10 is conv3_1 (3, 3, 128, 256)
-    11 is relu
-    12 is conv3_2 (3, 3, 256, 256)
-    13 is relu
-    14 is conv3_3 (3, 3, 256, 256)
-    15 is relu
-    16 is conv3_4 (3, 3, 256, 256)
-    17 is relu
-    18 is maxpool
-    19 is conv4_1 (3, 3, 256, 512)
-    20 is relu
-    21 is conv4_2 (3, 3, 512, 512)
-    22 is relu
-    23 is conv4_3 (3, 3, 512, 512)
-    24 is relu
-    25 is conv4_4 (3, 3, 512, 512)
-    26 is relu
-    27 is maxpool
-    28 is conv5_1 (3, 3, 512, 512)
-    29 is relu
-    30 is conv5_2 (3, 3, 512, 512)
-    31 is relu
-    32 is conv5_3 (3, 3, 512, 512)
-    33 is relu
-    34 is conv5_4 (3, 3, 512, 512)
-    35 is relu
-    36 is maxpool
-    37 is fullyconnected (7, 7, 512, 4096)
-    38 is relu
-    39 is fullyconnected (1, 1, 4096, 4096)
-    40 is relu
-    41 is fullyconnected (1, 1, 4096, 1000)
-    42 is softmax
+    Returns a model for the purpose of 'painting' the picture.
+
+    Takes only the convolution layer weights and wrap using the TensorFlow
+    Conv2d, Relu and AveragePooling layer. VGG actually uses maxpool but
+    the paper indicates that using AveragePooling yields better results.
+    The last few fully connected layers are not used.
+
+    Here is the detailed configuration of the VGG model:
+
+        0 is conv1_1 (3, 3, 3, 64)
+        1 is relu
+        2 is conv1_2 (3, 3, 64, 64)
+        3 is relu    
+        4 is maxpool
+        5 is conv2_1 (3, 3, 64, 128)
+        6 is relu
+        7 is conv2_2 (3, 3, 128, 128)
+        8 is relu
+        9 is maxpool
+        10 is conv3_1 (3, 3, 128, 256)
+        11 is relu
+        12 is conv3_2 (3, 3, 256, 256)
+        13 is relu
+        14 is conv3_3 (3, 3, 256, 256)
+        15 is relu
+        16 is conv3_4 (3, 3, 256, 256)
+        17 is relu
+        18 is maxpool
+        19 is conv4_1 (3, 3, 256, 512)
+        20 is relu
+        21 is conv4_2 (3, 3, 512, 512)
+        22 is relu
+        23 is conv4_3 (3, 3, 512, 512)
+        24 is relu
+        25 is conv4_4 (3, 3, 512, 512)
+        26 is relu
+        27 is maxpool
+        28 is conv5_1 (3, 3, 512, 512)
+        29 is relu
+        30 is conv5_2 (3, 3, 512, 512)
+        31 is relu
+        32 is conv5_3 (3, 3, 512, 512)
+        33 is relu
+        34 is conv5_4 (3, 3, 512, 512)
+        35 is relu
+        36 is maxpool
+        37 is fullyconnected (7, 7, 512, 4096)
+        38 is relu
+        39 is fullyconnected (1, 1, 4096, 4096)
+        40 is relu
+        41 is fullyconnected (1, 1, 4096, 1000)
+        42 is softmax
     """
     vgg = scipy.io.loadmat(path)
 
     vgg_layers = vgg['layers']
     def _weights(layer, expected_layer_name):
+        """
+        Return the weights and bias from the VGG model for a given layer.
+        """
         W = vgg_layers[0][layer][0][0][0][0][0]
         b = vgg_layers[0][layer][0][0][0][0][1]
         layer_name = vgg_layers[0][layer][0][0][-2]
@@ -78,21 +148,39 @@ def load_vgg_model(path):
         return W, b
 
     def _relu(conv2d_layer):
+        """
+        Return the RELU function wrapped over a TensorFlow layer. Expects a
+        Conv2d layer input.
+        """
         return tf.nn.relu(conv2d_layer)
 
     def _conv2d(prev_layer, layer, layer_name):
+        """
+        Return the Conv2D layer using the weights, biases from the VGG
+        model at 'layer'.
+        """
         W, b = _weights(layer, layer_name)
+        W = tf.constant(W)
+        b = tf.constant(np.reshape(b, (b.size)))
         return tf.nn.conv2d(
             prev_layer, filter=W, strides=[1, 1, 1, 1], padding='SAME') + b
 
     def _conv2d_relu(prev_layer, layer, layer_name):
+        """
+        Return the Conv2D + RELU layer using the weights, biases from the VGG
+        model at 'layer'.
+        """
         return _relu(_conv2d(prev_layer, layer, layer_name))
 
     def _avgpool(prev_layer):
+        """
+        Return the AveragePooling layer.
+        """
         return tf.nn.avg_pool(prev_layer, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
+    # Constructs the graph model.
     graph = {}
-    graph['input']   = tf.Variable(np.zeros((1, IMAGE_WIDTH, IMAGE_HEIGHT, COLOR_CHANNELS)), dtype = 'float32')
+    graph['input']   = tf.Variable(np.zeros((1, IMAGE_HEIGHT, IMAGE_WIDTH, COLOR_CHANNELS)), dtype = 'float32')
     graph['conv1_1']  = _conv2d_relu(graph['input'], 0, 'conv1_1')
     graph['conv1_2']  = _conv2d_relu(graph['conv1_1'], 2, 'conv1_2')
     graph['avgpool1'] = _avgpool(graph['conv1_2'])
@@ -115,51 +203,119 @@ def load_vgg_model(path):
     graph['conv5_4']  = _conv2d_relu(graph['conv5_3'], 34, 'conv5_4')
     graph['avgpool5'] = _avgpool(graph['conv5_4'])
     return graph
-    """
-    graph['conv1_1']  = _conv2d(graph['input'], 0, 'conv1_1')
-    graph['relu1_1']  = _relu(graph['conv1_1'])
-    graph['conv1_2']  = _conv2d(graph['relu1_1'], 2, 'conv1_2')
-    graph['relu1_2']  = _relu(graph['conv1_2'])
-    graph['avgpool1'] = _avgpool(graph['relu1_2'])
-    graph['conv2_1']  = _conv2d(graph['avgpool1'], 5, 'conv2_1')
-    graph['relu2_1']  = _relu(graph['conv2_1'])
-    graph['conv2_2']  = _conv2d(graph['relu2_1'], 7, 'conv2_2')
-    graph['relu2_2']  = _relu(graph['conv2_2'])
-    graph['avgpool2'] = _avgpool(graph['relu2_2'])
-    graph['conv3_1']  = _conv2d(graph['avgpool2'], 10, 'conv3_1')
-    """
 
+def content_loss_func(sess, model):
     """
-    vgg_layers = vgg['layers']
-    l = 0
-    tmp = vgg_layers[0][l][0][0][0][0][1]
-    print(tmp)
-    print(tmp.shape)
-    for l in range(43):
-        print('l = %d' % (l))
-        name = vgg_layers[0][l][0][0][-2]
-        print(name)
-        try:
-            mat = vgg_layers[0][l][0][0][0][0][0]
-            print(mat.shape)
-        except:
-            pass
+    Content loss function as defined in the paper.
     """
+    def _content_loss(p, x):
+        # N is the number of filters (at layer l).
+        N = p.shape[3]
+        # M is the height times the width of the feature map (at layer l).
+        M = p.shape[1] * p.shape[2]
+        # Interestingly, the paper uses this form instead:
+        #
+        #   0.5 * tf.reduce_sum(tf.pow(x - p, 2)) 
+        #
+        # But this form is very slow in "painting" and thus could be missing
+        # out some constants (from what I see in other source code), so I'll
+        # replicate the same normalization constant as used in style loss.
+        return (1 / (4 * N**2 * M**2)) * tf.reduce_sum(tf.pow(x - p, 2))
+    return _content_loss(sess.run(model['conv4_2']), model['conv4_2'])
 
-def main():
-    # content_image = load_image(CONTENT_IMAGE)
-    # style_image = load_image(STYLE_IMAGE)
-    load_vgg_model(VGG_MODEL)
+def style_loss_func(sess, model):
+    """
+    Style loss function as defined in the paper.
+    """
+    def _gram_matrix(F, N, M):
+        """
+        The gram matrix G.
+        """
+        Ft = tf.reshape(F, (M, N))
+        return tf.matmul(tf.transpose(Ft), Ft)
 
-    content_loss = 0
-    style_loss = 0
-    total_loss = BETA * content_loss + ALPHA * style_loss
-    
-    sys.exit()
+    def _style_loss(a, x):
+        """
+        The style loss calculation.
+        """
+        # N is the number of filters (at layer l).
+        N = a.shape[3]
+        # M is the height times the width of the feature map (at layer l).
+        M = a.shape[1] * a.shape[2]
+        # A is the style representation of the original image (at layer l).
+        A = _gram_matrix(a, N, M)
+        # G is the style representation of the generated image (at layer l).
+        G = _gram_matrix(x, N, M)
+        result = (1 / (4 * N**2 * M**2)) * tf.reduce_sum(tf.pow(G - A, 2))
+        return result
 
-    with tf.Session() as sess:
-        sess.run(tf.initialize_all_variables())
+    # Layers to use. We will use these layers as advised in the paper.
+    # To have softer features, increase the weight of the higher layers
+    # (conv5_1) and decrease the weight of the lower layers (conv1_1).
+    # To have harder features, decrease the weight of the higher layers
+    # (conv5_1) and increase the weight of the lower layers (conv1_1).
+    layers = [
+        ('conv1_1', 0.5),
+        ('conv2_1', 1.0),
+        ('conv3_1', 1.5),
+        ('conv4_1', 3.0),
+        ('conv5_1', 4.0),
+    ]
+
+    E = [_style_loss(sess.run(model[layer_name]), model[layer_name]) for layer_name, _ in layers]
+    W = [w for _, w in layers]
+    loss = sum([W[l] * E[l] for l in range(len(layers))])
+    return loss
 
 
 if __name__ == '__main__':
-    main()
+    with tf.Session() as sess:
+        # Load the images.
+        content_image = load_image(CONTENT_IMAGE)
+        style_image = load_image(STYLE_IMAGE)
+        # Load the model.
+        model = load_vgg_model(VGG_MODEL)
+
+        # Generate the white noise and content presentation mixed image
+        # which will be the basis for the algorithm to "paint".
+        input_image = generate_noise_image(content_image)
+
+        sess.run(tf.initialize_all_variables())
+        # Construct content_loss using content_image.
+        sess.run(model['input'].assign(content_image))
+        content_loss = content_loss_func(sess, model)
+
+        # Construct style_loss using style_image.
+        sess.run(model['input'].assign(style_image))
+        style_loss = style_loss_func(sess, model)
+
+        # Instantiate equation 7 of the paper.
+        total_loss = BETA * content_loss + ALPHA * style_loss
+
+        # From the paper: jointly minimize the distance of a white noise image
+        # from the content representation of the photograph in one layer of
+        # the neywork and the style representation of the painting in a number
+        # of layers of the CNN.
+        #
+        # The content is built from one layer, while the style is from five
+        # layers. Then we minimize the total_loss, which is the equation 7.
+        optimizer = tf.train.AdamOptimizer(2.0)
+        train_step = optimizer.minimize(total_loss)
+
+        sess.run(tf.initialize_all_variables())
+        sess.run(model['input'].assign(input_image))
+        for it in range(ITERATIONS):
+            sess.run(train_step)
+            if it%100 == 0:
+                # Print every 100 iteration.
+                mixed_image = sess.run(model['input'])
+                print('Iteration %d' % (it))
+                print('sum : ', sess.run(tf.reduce_sum(mixed_image)))
+                print('cost: ', sess.run(total_loss))
+
+                if not os.path.exists(OUTPUT_DIR):
+                    os.mkdir(OUTPUT_DIR)
+
+                filename = 'output/%d.png' % (it)
+                save_image(filename, mixed_image)
+
